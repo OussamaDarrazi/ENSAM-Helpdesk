67aee1be839411e708d1b3b423606e826ec04528
Oussama Darrazi <nostra.darrazi2002@gmail.com>
2024-05-18 01:31:55 +0100

    phone_number - condition on password

--- signup.php
+++ signup.php
@@ -11,38 +11,36 @@ $departments = $database->executeDQL("SELECT * from dept  order by dept_name = '
 
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    /*TODO: sign up:
-    case:
-     first-timer: generate code, send confirmation code, redirect to the confirmation page
-                 then for confirmation code: check code, if valid, create user, redirect to the login page
-     already registered: redirect to login page with error message
-    */
-    $email = strtolower($_POST["email"]);
-    $password = $_POST["password"];
-    $password2 = $_POST["password2"];
-    $first_name = ucfirst($_POST["first-name"]);
-    $last_name = ucfirst($_POST["last-name"]);
-    $department_id = $_POST["department"];
+    
 
-    $comptes_existant = $database->executeDQL("select * from employe where email='$email'");
-    if($comptes_existant){
-        if($comptes_existant[0]["isActive"]){
-            $errors[] = "Un compte actif est associé a cette adresse mail";
-        }else{
-            $errors[] = "Une tentative d'inscription est deja faite avec cette adresse mail, verifier votre boite mail pour activer votre compte.";
-        }
+        $email = strtolower($_POST["email"]);
+        $phone_number = $_POST["phone_number"];
+        $password = $_POST["password"];
+        $password2 = $_POST["password2"];
+        $first_name = ucfirst($_POST["first-name"]);
+        $last_name = ucfirst($_POST["last-name"]);
+        $department_id = $_POST["department"];
         
-    }else if($password != $password2){
-        $errors[] = "Les mots de passe ne se correspondent pas";
-    }else{
-        $pin = send_verification_pin($email);
-        $password_hash = password_hash($password, PASSWORD_DEFAULT); 
-        $database->executeDML("INSERT INTO verification_pin (email, pin) VALUES ('$email', '$pin');");
-        $database->executeDML("INSERT INTO employe 
-        (first_name, last_name, email, password, dept_id)
-        VALUES ('$first_name', '$last_name', '$email', '$password_hash', $department_id)");
+        $comptes_existant = $database->executeDQL("select * from employe where email='$email'");
+        if($comptes_existant){
+            if($comptes_existant[0]["isActive"]){
+                $errors[] = "Un compte actif est associé a cette adresse mail";
+            }else{
+                $errors[] = "Une tentative d'inscription est deja faite avec cette adresse mail, verifier votre boite mail pour activer votre compte.";
+            }    
+        }else if($password != $password2){
+            $errors[] = "Les mots de passe ne se correspondent pas";
+        }else if(strlen($password)){
+            $errors[] = "Le mot de passe doit contenir au moins 8 caractères";
+        }else{
+            $pin = send_verification_pin($email);
+            $password_hash = password_hash($password, PASSWORD_DEFAULT); 
+            $database->executeDML("INSERT INTO verification_pin (email, pin) VALUES ('$email', '$pin');");
+            $database->executeDML("INSERT INTO employe 
+        (first_name, last_name, email, phone_number , password, dept_id)
+        VALUES ('$first_name', '$last_name', '$email', '$phone_number' ,'$password_hash', $department_id)");
         header("Location: confirm.php?email=$email");
-
+        
     }
 }
 
