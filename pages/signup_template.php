f0c29761062768c35f87cab07a9ff6c367938b8b
Oussama Darrazi <nostra.darrazi2002@gmail.com>
2024-05-19 00:52:35 +0100

    toggle password visibility

--- pages/signup_template.php
+++ pages/signup_template.php
@@ -44,7 +44,7 @@ if (!empty($errors)) {
 ?>
       <!-- End Error Messages -->
       <!-- Form -->
-      <form method="post" class="my-5">
+      <form method="post" class="my-5"  data-hs-toggle-password-group="">
         <div class="mb-4">
           <label for="hs-hero-first-name-2" class="block text-sm font-medium"><span class="sr-only">Prenom
               </span></label>
@@ -84,26 +84,50 @@ if (!empty($errors)) {
             placeholder="Adresse mail" required>
         </div>
         <div class="mb-4">
-          <label for="hs-hero-email-2" class="block text-sm font-medium"><span class="sr-only">Telephone
+          <label for="hs-hero-phone" class="block text-sm font-medium"><span class="sr-only">Telephone
               </span></label>
-          <input name="phone_number" pattern= "\+[0-9]{12}" type="text" id="hs-hero-email-2"
+          <input name="phone_number" pattern= "\+[0-9]{12}" type="text" id="hs-hero-phone"
             class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
             placeholder="Telephone (+xxxxxxxxxxxx)" required>
         </div>
 
-        <div class="mb-4">
-          <label for="hs-hero-password-2" class="block text-sm font-medium"><span
+        <div class="mb-4 relative">
+          <label for="hs-hero-password-1" class="block text-sm font-medium"><span
               class="sr-only">Mot de passe</span></label>
-          <input name="password" type="password" id="hs-hero-password-2"
+          <input name="password" type="password" id="hs-hero-password-1"
             class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
             placeholder="Mot de passe" required>
+            <button type="button" data-hs-toggle-password='{
+      "target": ["#hs-hero-password-2", "#hs-hero-password-1"]
+    }' class="absolute top-0 end-0 p-3.5 rounded-e-md">
+      <svg class="flex-shrink-0 size-3.5 text-gray-400 " width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <path class="hs-password-active:hidden" d="M9.88 9.88a3 3 0 1 0 4.24 4.24"></path>
+        <path class="hs-password-active:hidden" d="M10.73 5.08A10.43 10.43 0 0 1 12 5c7 0 10 7 10 7a13.16 13.16 0 0 1-1.67 2.68"></path>
+        <path class="hs-password-active:hidden" d="M6.61 6.61A13.526 13.526 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61"></path>
+        <line class="hs-password-active:hidden" x1="2" x2="22" y1="2" y2="22"></line>
+        <path class="hidden hs-password-active:block" d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z"></path>
+        <circle class="hidden hs-password-active:block" cx="12" cy="12" r="3"></circle>
+      </svg>
+    </button>
         </div>
-        <div class="mb-4">
+        <div class="mb-4 relative">
           <label for="hs-hero-password-2" class="block text-sm font-medium"><span
               class="sr-only">Confirmer votre mot de passe</span></label>
           <input name="password2" type="password" id="hs-hero-password-2"
             class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
             placeholder="Confirmer votre mot de passe" required>
+            <button type="button" data-hs-toggle-password='{
+      "target": ["#hs-hero-password-2", "#hs-hero-password-1"]
+    }' class="absolute top-0 end-0 p-3.5 rounded-e-md">
+      <svg class="flex-shrink-0 size-3.5 text-gray-400 " width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <path class="hs-password-active:hidden" d="M9.88 9.88a3 3 0 1 0 4.24 4.24"></path>
+        <path class="hs-password-active:hidden" d="M10.73 5.08A10.43 10.43 0 0 1 12 5c7 0 10 7 10 7a13.16 13.16 0 0 1-1.67 2.68"></path>
+        <path class="hs-password-active:hidden" d="M6.61 6.61A13.526 13.526 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61"></path>
+        <line class="hs-password-active:hidden" x1="2" x2="22" y1="2" y2="22"></line>
+        <path class="hidden hs-password-active:block" d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z"></path>
+        <circle class="hidden hs-password-active:block" cx="12" cy="12" r="3"></circle>
+      </svg>
+    </button>
         </div>
 
         <div class="grid">
