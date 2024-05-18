5106a996bb6f8752646bf9ba3ad92bc8c95d1b36
Oussama Darrazi <nostra.darrazi2002@gmail.com>
2024-05-18 01:16:40 +0100

    sanitizing the input - sending verification pin

--- signup.php
+++ signup.php
@@ -2,6 +2,7 @@
 require_once ("pages/utils/base.php");
 require_once ("pages/utils/render_template.php");
 require_once ("core/EnsamHelpdeskDatabase.php");
+require_once ("utils/send_verification_pin.php");
 $errors = [];
 $database = HelpdeskDatabase::getInstance();
 $departments = $database->executeDQL("SELECT * from dept  order by dept_name = 'Autre', dept_name");  //to keep Other hia l'option lkhra
@@ -16,19 +17,32 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                  then for confirmation code: check code, if valid, create user, redirect to the login page
      already registered: redirect to login page with error message
     */
-    $email = $_POST["email"];
+    $email = strtolower($_POST["email"]);
     $password = $_POST["password"];
     $password2 = $_POST["password2"];
-    $first_name = $_POST["first-name"];
-    $last_name = $_POST["last-name"];
+    $first_name = ucfirst($_POST["first-name"]);
+    $last_name = ucfirst($_POST["last-name"]);
     $department_id = $_POST["department"];
 
-    if($database->executeDQL("select * from employe where email='$email'")){
-        $errors[] = "L'adresse email est déjà utilisée";
+    $comptes_existant = $database->executeDQL("select * from employe where email='$email'");
+    if($comptes_existant){
+        if($comptes_existant[0]["isActive"]){
+            $errors[] = "Un compte actif est associé a cette adresse mail";
+        }else{
+            $errors[] = "Une tentative d'inscription est deja faite avec cette adresse mail, verifier votre boite mail pour activer votre compte.";
+        }
+        
     }else if($password != $password2){
         $errors[] = "Les mots de passe ne se correspondent pas";
     }else{
-        #TODO: Handle the signing up
+        $pin = send_verification_pin($email);
+        $password_hash = password_hash($password, PASSWORD_DEFAULT); 
+        $database->executeDML("INSERT INTO verification_pin (email, pin) VALUES ('$email', '$pin');");
+        $database->executeDML("INSERT INTO employe 
+        (first_name, last_name, email, password, dept_id)
+        VALUES ('$first_name', '$last_name', '$email', '$password_hash', $department_id)");
+        header("Location: confirm.php?email=$email");
+
     }
 }
 
