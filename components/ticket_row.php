d16323807c8dc5abb1ad3ff4fe62ffcb1c2dff45
Oussama Darrazi <nostra.darrazi2002@gmail.com>
2024-05-20 11:52:13 +0100

    changing components to be included instead of invoked functions

--- components/ticket_row.php
+++ components/ticket_row.php
@@ -1,29 +1,52 @@
 <?php
-function ticket_row(Ticket $ticket){
-    return "<tr>
-    <td class='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800'>
-    $ticket->id
-    </td>
-    <td class='px-6 py-4 whitespace-nowrap text-sm text-gray-800 text-ellipsis'>
-    $ticket->subject
-    </td>
-    <td class='px-6 py-4 whitespace-nowrap text-sm text-gray-800'>
-    $ticket->status_id
-    </td>
-    <td class='px-6 py-4 whitespace-nowrap text-sm text-gray-800'>
-    $ticket->category_id
-    </td>
-    <td class='px-6 py-4 whitespace-nowrap text-sm text-gray-800'>
-      helpdesk dropdown
-    </td>
-    <td class='px-6 py-4 whitespace-nowrap text-sm text-gray-800'>
-    $ticket->owner_id
-    </td>
-    <td class='px-6 py-4 whitespace-nowrap text-sm text-gray-800'>".
-    $ticket->created_at->format('Y/m/d H:i')
-    ."</td>
-    <td class='px-6 py-4 whitespace-nowrap text-sm text-gray-800'>".
-    $ticket->last_updated_at->format('Y/m/d H:i')
-    ."</td>
-  </tr>";
-}
+$status_colors = [
+  "bg-blue-600",
+  "bg-gray-500",
+  "bg-yellow-500",
+  "bg-teal-500"
+];
+$status_color = $status_colors[$ticket->status_id];
+?>
+
+<tr>
+  <td class='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800'>
+    <?= $ticket->id ?>
+  </td>
+  <td class='px-6 py-4 whitespace-nowrap text-sm text-gray-800 text-ellipsis'>
+    <?= $ticket->subject ?>
+  </td>
+  <td class='px-6 py-4 whitespace-nowrap text-sm text-gray-800'>
+
+    <span
+      class='inline-flex items-center gap-x-1.5 py-1.5 px-3 rounded-full text-xs font-medium $status_color text-white dark:bg-blue-500'>
+      <?= $ticket->status ?>
+    </span>
+  </td>
+  <td class='px-6 py-4 whitespace-nowrap text-sm text-gray-800'>
+    <?= $ticket->category ?>
+  </td>
+  <td class='px-6 py-4 whitespace-nowrap text-sm text-gray-800'>
+    <?php
+    if ($current_user->user_type == UserType::ADMIN) { ?>
+    <select name="assign" id="assign" class="py-3 px-4 pe-9 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none ">
+  
+        <option value="">Unassigned</option>
+        <?php foreach ($helpdesk as $hd) { ?>
+          <option value="<?= $hd->id?>"><?= $hd->first_name . " " . $hd->last_name ?></option>
+        <?php }
+        ?>
+      </select>
+
+    <?php }
+    ?>
+  </td>
+  <td class='px-6 py-4 whitespace-nowrap text-sm text-gray-800'>
+    <?= $ticket->owner_id ?>
+  </td>
+  <td class='px-6 py-4 whitespace-nowrap text-sm text-gray-800'>
+    <?= $ticket->created_at->format('Y/m/d H:i') ?>
+  </td>
+  <td class='px-6 py-4 whitespace-nowrap text-sm text-gray-800'>
+    <?= $ticket->last_updated_at->format('Y/m/d H:i') ?>
+  </td>
+</tr>
--- components/ticket_table.php
+++ components/ticket_table.php
@@ -1,11 +1,4 @@
-<?php
-require_once("components/ticket_row.php");
-function ticket_table($tickets){
-    $ticket_rows=[];
-    foreach($tickets as $ticket){
-        $ticket_rows[] = ticket_row($ticket);
-    }
-    return "<table class='max-w-full divide-y divide-gray-200 '>
+<table class='max-w-full divide-y divide-gray-200 '>
     <thead class='sticky top-0 bg-gray-50'>
       <tr>
         <th scope='col' class='px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase'># Ticket</th>
@@ -18,9 +11,11 @@ function ticket_table($tickets){
         <th scope='col' class='px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase'>Dernier MàJ à</th>
       </tr>
     </thead>
-    <tbody class='divide-y divide-gray-200 overflow-y-scroll'>".
-implode($ticket_rows)
-    ."</tbody>
-  </table>";
+    <tbody class='divide-y divide-gray-200 overflow-y-scroll'>
+      <?php
+      foreach($tickets as $ticket){
+        include("ticket_row.php");
+      }?>
+</tbody>
+  </table>"
 
-}
