c45ada6c0eb5b5fa145c9250edabce6dc964b752
Oussama Darrazi <nostra.darrazi2002@gmail.com>
2024-05-21 12:46:19 +0100

    ticket creation

--- create_ticket.php
+++ create_ticket.php
@@ -16,7 +16,26 @@ if(isset($_SESSION["user_id"])){
     header("Location: login.php");
 }
 
+//Ticket Submition
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $owner_id = $current_user->id;
+    $assigned_hd = null;
+    $created_at = new DateTime();
+    $last_updated_at = $created_at;
+    $subject = trim($_POST["ticket_subject"]);
+    $description = trim($_POST["ticket_description"]); //message
+    $category = $_POST["ticket_category"];
+    $status = $_POST["ticket_status"];
+    $ticket = new Ticket($owner_id, $assigned_hd, $created_at, $last_updated_at, $subject, $category, $status);
+    $ticket->save();
+    //Inserting the chat massage
+    $database->executeDML("INSERT INTO chat_message 
+                                (message_datetime, sender_id, ticket_id, content) 
+                                VALUES (?,?,?,?)", [$created_at->format('Y-m-d H:i:s'),  $current_user->id, $ticket->id, $description]);
+    echo $ticket->id;
+}
+
 $categories = $database->executeDQL("SELECT * from ticket_category ORDER BY category_name='Autre', category_name");
 $context["categories"] = $categories;
 
-echo base_template(render_template("pages/create_ticket_template.php", $context));
+echo base_template(render_template("pages/create_ticket_template.php", $context), "Nouveau Ticket");
