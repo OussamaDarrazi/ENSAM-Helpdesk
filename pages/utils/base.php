70196f1a41acc6dbffa7d042347a101ec6ba4101
Oussama Darrazi <nostra.darrazi2002@gmail.com>
2024-05-21 12:48:01 +0100

    htmx added letsgo

--- pages/utils/base.php
+++ pages/utils/base.php
@@ -14,11 +14,12 @@ function base_template($content, $title = "ENSAM Helpdesk")
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="src/output.css">
+        <script src="https://unpkg.com/htmx.org@1.9.12" integrity="sha384-ujb1lZYygJmzgSwoxRggbCHcjc0rB2XoQrxeTUQyRjrOnlCoYta87iKBWq3EsdM2" crossorigin="anonymous"></script>
         <link rel="shortcut icon" href="http://www.ensam-casa.ma/assets/images/ensam2.png" type="image/x-icon">
         <title><?= "ENSAM Helpdesk - " . $title ?></title>
     </head>
 
-    <body>
+    <body  class="min-w-screen min-h-screen ">
         <?= $content ?>
         <script src="./node_modules/preline/dist/preline.js"></script>
     </body>
