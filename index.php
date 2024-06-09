d453f26148328fd8daa901285f9160229a8d3204
Oussama Darrazi <nostra.darrazi2002@gmail.com>
2024-06-09 18:36:14 +0100

    employe/helpdesk stats

--- index.php
+++ index.php
@@ -17,20 +17,25 @@ if(isset($_SESSION["user_id"])){
     $context["articles"] = $articles;
     $context["categories"] = $categories;
 
-    $stats = $database->executeDQL("SELECT * FROM daily_stats")[0];
-    $context["stats"] = $stats;
     $helpdesk = User::UsersFromDB("SELECT * from employe where isHelpdesk=1 ORDER BY first_name");
     $context["helpdesk"] = $helpdesk;
     if($current_user->user_type == UserType::ADMIN){
+        $stats = $database->executeDQL("SELECT * FROM daily_stats")[0];
+        $context["stats"] = $stats;
         $tickets = Ticket::TicketsFromDB("SELECT * from ticket order by created_at desc");
         $context["tickets"] = $tickets;
         echo base_template(render_template("pages/admin_dashboard.php", $context), "Admin Dashboard");
     }elseif($current_user->user_type == UserType::HELPDESK){
         $tickets = Ticket::TicketsFromDB("SELECT * from ticket where assigned_to = ".$current_user->id." order by created_at desc");
+        $stats = $database->executeDQL("SELECT * FROM daily_helpdesk_stats where helpdesk_id = ?", [$current_user->id]);
+        $context["stats"] = $stats? $stats[0]:["AHT"=>"N/A","resolved_tickets"=>0];
         $context["tickets"] = $tickets;
         echo base_template(render_template("pages/helpdesk_dashboard.php", $context), "Helpdesk Dashboard");
     }elseif($current_user->user_type == UserType::EMPLOYE){
         $tickets = Ticket::TicketsFromDB("SELECT * from ticket where created_by = ".$current_user->id." order by created_at desc");
+        $stats = $database->executeDQL("SELECT * FROM daily_employe_stats where owner_id = ?", [$current_user->id]);
+        $context["stats"] = $stats? $stats[0]:["open_tickets"=>0,"resolved_tickets"=>0];
+
         $context["tickets"] = $tickets;
         echo base_template(render_template("pages/customer_dashboard.php", $context), "Employé Dashboard");
     }
