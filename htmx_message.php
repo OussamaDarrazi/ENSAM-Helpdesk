66d7ed20769ad5f8775045c9b50aac6952b95b0f
Oussama Darrazi <nostra.darrazi2002@gmail.com>
2024-06-10 11:28:55 +0100

    unnecessary login redirection

--- htmx_message.php
+++ htmx_message.php
@@ -11,17 +11,14 @@ if (isset($_SESSION["user_id"])) {
     $id = $_SESSION["user_id"];
     $current_user = User::UsersFromDB("SELECT * FROM employe where employe_id = $id")[0];
     $context["current_user"] = $current_user;
-} else {
-        header("Location: login.php");
-}
-
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    if(!empty($_POST["message"])){   
-        $message = nl2br(htmlspecialchars($_POST["message"]));
-        $message = new Message($message, $current_user->id, $ticket_id, new DateTime());
-        $message->save();
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (!empty($_POST["message"])) {
+            $message = nl2br(htmlspecialchars($_POST["message"]));
+            $message = new Message($message, $current_user->id, $ticket_id, new DateTime());
+            $message->save();
+        }
+        $latest_messages = Message::MessagesFromDB("SELECT * FROM chat_message WHERE ticket_id = $ticket_id ORDER BY message_datetime");
+        $context["latest_messages"] = $latest_messages;
+        include ("components/message_list.php");
     }
-    $latest_messages = Message::MessagesFromDB("SELECT * FROM chat_message WHERE ticket_id = $ticket_id ORDER BY message_datetime");
-    $context["latest_messages"] = $latest_messages;
-    include("components/message_list.php");
 }
